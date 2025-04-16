85eaa18a30c119b6083f4a495562867e209e658a
solomon-ctg <solomon.iroegbu@gmail.com>
2025-04-16 19:04:20 +0100

    Add New Design And Blog

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::view('/second', 'second');
 Route::view('/', 'home')->name('home');
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
+Route::view('article', 'article')->name('article');
