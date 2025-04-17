f6080bb2231dfdd6d2487ad12d4a8b0a447f7b04
solomon-ctg <solomon.iroegbu@gmail.com>
2025-04-17 16:37:07 +0100

    feat: Create CRUD: Index, Create, Update, Delete

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,25 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PostController;
-use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
-//Route::view('/', 'home');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 
-//Route::get('/second', function () {
- //   return view('second');
-//});
+Route::resource('categories', CategoryController::class);
 
-// More simple syntax:
-Route::view('/second', 'second');
 
-// Route::view('/', 'home')->name('home');
-//Route::get('/', function () {
-//    $categories = DB::table('categories')->get();
-//    return view('home', ['categories' => $categories]);
-//})->name('home');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/', [HomeController::class, 'index'])
-    ->name('home');
-
-//Route::get('posts/{postId}', [PostController::class, 'show'])
-//    ->name('post.show');
-Route::get('posts/{post}', [PostController::class, 'show'])
-    ->name('post.show');
-
-Route::view('contact', 'contact')->name('contact');
-Route::view('about', 'about')->name('about');
-Route::view('article', 'article')->name('article');
+require __DIR__.'/auth.php';
