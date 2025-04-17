3411e25865d5f0c21898a69762c8b2a4a756f8a2
solomon-ctg <solomon.iroegbu@gmail.com>
2025-04-17 17:11:16 +0100

    feat: Post CRUD: Perfomance and Debugbar

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\IsAdminMiddleware;
 use Illuminate\Support\Facades\Route;
@@ -24,8 +25,12 @@ Route::middleware('auth')->group(function () {
         return view('dashboard');
     })->middleware(['verified'])->name('dashboard');
 
-    Route::resource('categories', CategoryController::class)
-        ->middleware(IsAdminMiddleware::class);
+
+
+    Route::middleware(IsAdminMiddleware::class)->group(function () {
+        Route::resource('categories', CategoryController::class);
+        Route::resource('posts', PostController::class);
+    });
 
 });
 
