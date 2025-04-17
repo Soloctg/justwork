e48088cbf2d9befee35b83fbf3fc6c8ac13165b8
solomon-ctg <solomon.iroegbu@gmail.com>
2025-04-17 16:43:27 +0100

    feat: Admin User, Route Groups, Middleware

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,14 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\IsAdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 
 Route::resource('categories', CategoryController::class);
@@ -20,6 +19,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['verified'])->name('dashboard');
+
+    Route::resource('categories', CategoryController::class)
+        ->middleware(IsAdminMiddleware::class);
+
 });
 
 require __DIR__.'/auth.php';
