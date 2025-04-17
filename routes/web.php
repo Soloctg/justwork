0f6c66cf26dfdc092ba5db0decd6e3d8f0da1e7b
solomon-ctg <solomon.iroegbu@gmail.com>
2025-04-17 12:19:04 +0100

    Feat: Add MVC, DB Queries and Eloquent Models

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+
 
 //Route::get('/', function () {
 //    return view('welcome');
@@ -16,7 +19,15 @@ use Illuminate\Support\Facades\Route;
 // More simple syntax:
 Route::view('/second', 'second');
 
-Route::view('/', 'home')->name('home');
+// Route::view('/', 'home')->name('home');
+//Route::get('/', function () {
+//    $categories = DB::table('categories')->get();
+//    return view('home', ['categories' => $categories]);
+//})->name('home');
+
+Route::get('/', [HomeController::class, 'index'])
+    ->name('home');
+
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
 Route::view('article', 'article')->name('article');
